5499864a52d4a329b8abaac92dadc0cf646c6775
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-10 19:52:55 -0500

    feat(public): Added sidenav + toolbar + styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {RouterOutlet} from '@angular/router';
 import {TranslateService} from "@ngx-translate/core";
 import {LanguageSwitcherComponent} from "./public/components/language-switcher/language-switcher.component";
 import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import {SideNavigationBarComponent} from "./public/components/side-navigation-bar/side-navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LanguageSwitcherComponent, FooterContentComponent],
+  imports: [RouterOutlet, LanguageSwitcherComponent, FooterContentComponent, SideNavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/public/components/side-navigation-bar/side-navigation-bar.component.ts
+++ src/app/public/components/side-navigation-bar/side-navigation-bar.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {LanguageSwitcherComponent} from "../language-switcher/language-switcher.component";
+import {FooterContentComponent} from "../footer-content/footer-content.component";
 
 @Component({
   selector: 'app-side-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatSidenav,
+    MatToolbar,
+    MatIconButton,
+    MatIcon,
+    LanguageSwitcherComponent,
+    FooterContentComponent
+  ],
   templateUrl: './side-navigation-bar.component.html',
   styleUrl: './side-navigation-bar.component.css'
 })
